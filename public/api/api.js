79878a23551a3472c6b39ea3acdd0de8d74d01e4
Omar Alolayan <omar@Omars-MacBook-Pro.local>
2018-11-29 17:32:36 -0500

    adding variables to make life easier

--- public/api/api.js
+++ public/api/api.js
@@ -1,4 +1,12 @@
 
+function calc_pref(){
+
+
+
+    return preffered ;
+}
+
+
 function create() {
 
     var obj = {
@@ -44,7 +52,6 @@ function submituserNames() {
 
         //There should be a function here returning prefrence
 
-        var preffered = 'subway';
 
         var temp20 = '/usersLoc';
         var temp21 = '/';
@@ -54,16 +61,50 @@ function submituserNames() {
         var temp29 = temp20+temp21+temp22+temp21+temp23+temp21+temp24;
         var url = temp29;
 
-        
-         superagent
+        superagent
             .get(url)
             .end(function(err,res){
                 if(err){ console.log(err);}
                 else {
-                var result =  res.body;
-                var  avg_latq = res.body.avg_lat;
-                var  avg_longq = res.body.avg_long;
-                window.location.href = "../result.html?avg_lat="+avg_latq+"&avg_long="+avg_longq+"&preffered="+preffered;
+                var bulk_data =  res.body;
+                
+
+                var u1Lat = bulk_data.firstuserNameData.userLat;
+                var u1Long = bulk_data.firstuserNameData.userLong;
+                var u2Lat = bulk_data.seconduserNameData.userLat;
+                var u2Long = bulk_data.seconduserNameData.userLong;
+                var u3Lat = bulk_data.thirduserNameData.userLat;
+                var u3Long = bulk_data.thirduserNameData.userLong;
+            
+                avg_lat  = (Number(u1Lat) + Number(u2Lat) + Number(u3Lat))/3;
+                avg_long = (Number(u1Long)+Number(u2Long)+Number(u3Long))/3;
+
+                // User1 pref resturants as strings
+                var u1p1 = bulk_data.firstuserNameData.res1;
+                var u1p2 = bulk_data.firstuserNameData.res2;
+                var u1p3 = bulk_data.firstuserNameData.res3;
+                var u1p4 = bulk_data.firstuserNameData.res4;
+                var u1p5 = bulk_data.firstuserNameData.res5;
+                
+                // User2 pref resturants as strings
+                var u2p1 = bulk_data.seconduserNameData.res1;
+                var u2p2 = bulk_data.seconduserNameData.res2;
+                var u2p3 = bulk_data.seconduserNameData.res3;
+                var u2p4 = bulk_data.seconduserNameData.res4;
+                var u2p5 = bulk_data.seconduserNameData.res5;
+                                
+                // User3 pref resturants as strings
+                var u3p1 = bulk_data.thirduserNameData.res1;
+                var u3p2 = bulk_data.thirduserNameData.res2;
+                var u3p3 = bulk_data.thirduserNameData.res3;
+                var u3p4 = bulk_data.thirduserNameData.res4;
+                var u3p5 = bulk_data.thirduserNameData.res5;
+                
+                // for Mason : write your function here using the varaibles above and store it in var preffered = 'subway';
+                
+                var preffered = 'subway';
+
+                window.location.href = "../result.html?avg_lat="+avg_lat+"&avg_long="+avg_long+"&preffered="+preffered;
 
             }
             })
